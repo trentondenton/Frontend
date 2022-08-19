093c4dd1ae2c40c011fca24391adb7cf0d475237
Trenton <trentondenton91@gmail.com>
2022-08-19 13:47:07 -0500

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const config = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, 'css-loader', 'sass-loader'],
+        use: ['css-loader', 'sass-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
